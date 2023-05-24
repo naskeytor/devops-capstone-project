0f2a65931c73a4cbe6551e211815a3d2a6d82be2
naskeytor <103206403+naskeytor@users.noreply.github.com>
2023-05-24 19:42:16 +0200

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -63,7 +63,7 @@ def create_accounts():
 
 # ... place you code here to LIST accounts ...
 
-    
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -106,7 +106,7 @@ def get_accounts(account_id):
 
 # ... place you code here to UPDATE an account ...
 
-    
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -131,7 +131,7 @@ def update_accounts(account_id):
 
 # ... place you code here to DELETE an account ...
 
-    
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
